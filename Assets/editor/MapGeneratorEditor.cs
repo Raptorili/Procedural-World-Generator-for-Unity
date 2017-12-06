fae36ce0198fc73ac3f5cb0e1357229fac60bdc1
Drew Vasili <Vasili.andrew@gmail.com>
2017-12-06 20:33:57 +0000

    Noise map creation
    Adding generation of a Noise map

---
+++ Assets/editor/MapGeneratorEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGeneration))]
+public class MapGeneratorEditor : Editor {
+
+	public override void OnInspectorGUI(){
+
+		MapGeneration mapGen = (MapGeneration)target;
+
+		if (DrawDefaultInspector ()) {
+			if (mapGen.autoUpdate) {
+				mapGen.GenerateMap ();
+			}
+		}
+
+		if(GUILayout.Button("Generate")){
+			mapGen.GenerateMap();
+		}
+	}
+}
