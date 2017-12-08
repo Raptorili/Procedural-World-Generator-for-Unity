cd2dd464d9c83136570182252470453ee88135a8
Drew Vasili <Vasili.andrew@gmail.com>
2017-12-08 13:56:34 +0000

    Gradient Editor
    Started the creation of a gradient editor and finished the color
    version of the noise map.

---
+++ Assets/scripts/ColorSchemePicker.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSchemePicker : MonoBehaviour {
+
+    public CustomGradient myGradient;
+    public CustomGradient otherGradient;
+}
